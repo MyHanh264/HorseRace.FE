957335ab4418647e723a9903f9eae128db4aa629
MyHanh264 <hanhttmse180597@fpt.edu.vn>
2026-06-17 14:39:13 +0700

    feat: horse owner page - side bar, register/view/edit horse modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AuthSessionSync from './components/AuthSessionSync'
-import RequireRole from './components/RequireRole'
-import ForgotPasswordPage from './pages/loginPage/ForgotPasswordPage'
-import LoginPage from './pages/loginPage/LoginPage'
-import ResetPasswordPage from './pages/loginPage/ResetPasswordPage'
-import RegisterPage from './pages/registerPage/RegisterPage'
-import LandingDashboard from './pages/customer/LandingDashboard'
-import AdminDashboard from './pages/admin/AdminDashboard'
-import SpectatorDashboard from './pages/spectator/SpectatorDashboard'
-import JockeyDashboard from './pages/jockey/JockeyDashboard'
-import HorseOwnerDashboard from './pages/horse-owner/HorseOwnerDashboard'
-import RefereeDashboard from './pages/referee/RefereeDashboard'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthSessionSync from "./components/AuthSessionSync";
+import RequireRole from "./components/RequireRole";
+
+// Auth Pages
+import LoginPage from "./pages/loginPage/LoginPage";
+import ForgotPasswordPage from "./pages/loginPage/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/loginPage/ResetPasswordPage";
+import RegisterPage from "./pages/registerPage/RegisterPage";
+
+// Public Pages
+import LandingDashboard from "./pages/customer/LandingDashboard";
+
+// Admin
+import AdminDashboard from "./pages/admin/AdminDashboard";
+
+// Spectator
+import SpectatorDashboard from "./pages/spectator/SpectatorDashboard";
+
+// Jockey
+import JockeyDashboard from "./pages/jockey/JockeyDashboard";
+
+// Referee
+import RefereeDashboard from "./pages/referee/RefereeDashboard";
+
+// Horse Owner
+import HorseOwnerDashboard from "./pages/horse-owner/HorseOwnerDashboard";
+import HorseOwnerLayout from "./components/layout/HorseOwnerLayout";
+import MyHorsesPage from "./pages/horse-owner/MyHorsesPage";
+import MyEntriesPage from "./pages/horse-owner/MyEntriesPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <AuthSessionSync />
+      <AuthSessionSync /> {/* ← đứng một mình, không bọc Routes */}
       <Routes>
+        {/* Public */}
         <Route path="/" element={<LandingDashboard />} />
-
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
         <Route path="/register" element={<RegisterPage />} />
 
+        {/* Admin */}
         <Route
           path="/admin"
           element={
@@ -33,6 +51,8 @@ function App() {
             </RequireRole>
           }
         />
+
+        {/* Spectator */}
         <Route
           path="/spectator"
           element={
@@ -41,6 +61,8 @@ function App() {
             </RequireRole>
           }
         />
+
+        {/* Jockey */}
         <Route
           path="/jockey"
           element={
@@ -49,14 +71,8 @@ function App() {
             </RequireRole>
           }
         />
-        <Route
-          path="/horse-owner"
-          element={
-            <RequireRole role="HORSE_OWNER">
-              <HorseOwnerDashboard />
-            </RequireRole>
-          }
-        />
+
+        {/* Referee */}
         <Route
           path="/referee"
           element={
@@ -65,9 +81,23 @@ function App() {
             </RequireRole>
           }
         />
+
+        {/* Horse Owner */}
+        <Route
+          path="/horse-owner"
+          element={
+            <RequireRole role="HORSE_OWNER">
+              <HorseOwnerLayout />
+            </RequireRole>
+          }
+        >
+          <Route index element={<HorseOwnerDashboard />} />
+          <Route path="horses" element={<MyHorsesPage />} />
+          <Route path="entries" element={<MyEntriesPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
